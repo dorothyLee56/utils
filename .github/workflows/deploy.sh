a53074b59519f00264153ca9b7e66cc297102e06
dingyanping <dingyanping@360.cn>
2023-08-09 00:50:20 +0800

    feat: edit deploy
    Signed-off-by: dingyanping <dingyanping@360.cn>

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dorothyLee56/utils.git master:gh-pages
+git push -f git@github.com:dorothyLee56/utils.git main:gh-pages
 
 cd -
